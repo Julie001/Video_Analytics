a19ac983cb16af257c0bdb36e975709a8afe58de
Julie001 <britainpounds@gmail.com>
2017-01-30 13:49:41 +0000

    Update Face_Detection_Video.py
    face detection codes before adding the Print(count) testing lines

--- Face_Detection_Video.py
+++ Face_Detection_Video.py
@@ -9,19 +9,18 @@ cap = cv2.VideoCapture()
 cap.open(fp)
 
 face_cascade = cv2.CascadeClassifier(fp2)
-count = 0
+
 while True:
     ret, frame = cap.read()
 
     if ret == True:
-        count +=1
-        print(count)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray,1.3, 8)
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        
-    
+        cv2.imshow('Video Feed at FLE Instance....',frame)
+        cv2.waitKey(25)
+
     else:
         break
 
